98ca25155d1bfb63c1aad88d6147072acf9fb251
Haythem Elj <haythem.elj@gfi-tunisie.com>
2020-09-28 14:38:39 +0200

    SepaPurchase

---
+++ CryptoCoin.UI.Components.Sepa/SepaComponent.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoCoin.UI.Components.Sepa
+{
+     public partial class SepaComponent : ComponentBase
+    {
+       
+
+        static CreancierParameters crea = new CreancierParameters()
+        {
+            creancierID = "FR21542125852",
+            creancierName = "France Digital Consulting",
+            creancierAdresse = "3 RUE MRX DORMOY 91480 Quincy Sous senart",
+            creancierPays = "France"
+        };
+        SepaParameters sepa = new SepaParameters()
+        {
+            creancier = crea,
+            debiteur = new DebiteurParameters(),
+            referenceMandat = "XXXXXXXXXXXX001"
+        };
+
+        public void OnSubmitSepa()
+        {
+
+
+        }
+
+    }
+}
---
+++ CryptoCoin.UI.Components.Sepa/SepaParameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoCoin.UI.Components.Sepa
+{
+   public class SepaParameters
+    {
+        public string referenceMandat { get; set; }
+        public DebiteurParameters debiteur { get; set; }
+        public CreancierParameters creancier { get; set; }
+    }
+}
